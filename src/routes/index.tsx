e2927b9ab6c89335a22cea1f4ad489e65515048f
hushin <ngtv.hsn@gmail.com>
2025-03-21 15:41:39 +0900

    feat: カードコンポーネントを追加し、ホームコンポーネントに統合

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router';
 
 import { Button } from '~shared/ui/button';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from '~shared/ui/card';
 
 export const Route = createFileRoute('/')({
   component: HomeComponent,
@@ -11,6 +19,18 @@ function HomeComponent() {
     <div className="p-2">
       <h3>Welcome Home!</h3>
       <Button>Click Me</Button>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
