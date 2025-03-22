04c1d1cd8870bdeb25b5227d0eadb0560a8e66d5
hushin <ngtv.hsn@gmail.com>
2025-03-22 13:15:12 +0900

    feat: MSW ストーリーブックアドオンを追加し、グローバルに初期化設定を実装

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,1 +1,13 @@
+import { initialize, mswLoader } from 'msw-storybook-addon';
 import '../src/styles.css';
+
+// Initialize MSW
+initialize();
+
+const preview = {
+  parameters: {},
+  // Provide the MSW addon loader globally
+  loaders: [mswLoader],
+};
+
+export default preview;
