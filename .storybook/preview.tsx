23c86e02ab6ca61adf6bd7d4332e1e966785f2b9
hushin <ngtv.hsn@gmail.com>
2025-03-22 13:55:48 +0900

    feat: React Query のクエリをリセットするコンポーネントを追加し、Storybook のプレビューに統合

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,11 +1,23 @@
 import { initialize, mswLoader } from 'msw-storybook-addon';
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../src/styles.css';
 
 import type { Preview } from '@storybook/react';
+import { useQueryClient } from '@tanstack/react-query';
 import { Providers } from '../src/shared/lib/providers';
 
 initialize();
+
+const ResetQueries = () => {
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    return () => {
+      queryClient.resetQueries();
+    };
+  }, [queryClient]);
+  return null;
+};
 
 const preview: Preview = {
   parameters: {},
@@ -15,6 +27,7 @@ const preview: Preview = {
     (Story) => (
       <Providers>
         <Story />
+        <ResetQueries />
       </Providers>
     ),
   ],
