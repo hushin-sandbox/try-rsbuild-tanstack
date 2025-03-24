a8101ab248c65729d2b33a4a8fef91e259133848
hushin <ngtv.hsn@gmail.com>
2025-03-24 20:51:06 +0900

    feat: JSDOMのPointerEventをモックする実装を追加

--- test/setup.ts
+++ test/setup.ts
@@ -21,3 +21,28 @@ Object.defineProperty(globalThis, 'crypto', {
 
 // テスト用の固定時間を設定
 vi.setSystemTime(new Date('2025-03-21T00:00:00.000Z'));
+
+/**
+ * JSDOM doesn't implement PointerEvent so we need to mock our own implementation
+ * Default to mouse left click interaction
+ * https://github.com/radix-ui/primitives/issues/1822
+ * https://github.com/jsdom/jsdom/pull/2666
+ */
+class MockPointerEvent extends Event {
+  button: number;
+  ctrlKey: boolean;
+  pointerType: string;
+
+  constructor(type: string, props: PointerEventInit) {
+    super(type, props);
+    this.button = props.button || 0;
+    this.ctrlKey = props.ctrlKey || false;
+    this.pointerType = props.pointerType || 'mouse';
+  }
+}
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+window.PointerEvent = MockPointerEvent as any;
+window.HTMLElement.prototype.scrollIntoView = vi.fn();
+window.HTMLElement.prototype.releasePointerCapture = vi.fn();
+window.HTMLElement.prototype.hasPointerCapture = vi.fn();
